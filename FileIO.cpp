15eeae177beb281231d9cf9c945adc536c8003b8
Cyrille Berger <cberger@cberger.net>
2019-01-26 15:29:23 +0100

    monitor file changes

--- FileIO.cpp
+++ FileIO.cpp
@@ -4,8 +4,11 @@
 #include <QFile>
 #include <QQmlEngine>
 #include <QFileInfo>
+#include <QFileSystemWatcher>
 
 #include <qqml.h>
+
+QFileSystemWatcher* FileIO::s_watcher = nullptr;
 
 FileIO::FileIO(QObject *parent) :
   QObject(parent)
@@ -46,4 +49,55 @@ void FileIO::addImportPath(const QUrl& _url)
 void FileIO::setCurrentPath(const QUrl& _url)
 {
   QDir::setCurrent(QFileInfo(_url.toLocalFile()).absolutePath());
+}
+
+void FileIO::setUrl(const QUrl& _url)
+{
+  if(_url == m_url) return;
+  if(m_monitorChanges)
+  {
+    if(not m_url.toLocalFile().isEmpty())
+    {
+      s_watcher->removePath(m_url.toLocalFile());
+    }
+    if(not _url.toLocalFile().isEmpty())
+    {
+      s_watcher->addPath(_url.toLocalFile());
+    }
+  }
+  m_url = _url;
+  emit(urlChanged());
+}
+
+void FileIO::setMonitorChanges(bool _v)
+{
+  if(m_monitorChanges == _v) return;
+  if(_v)
+  {
+    if(not s_watcher)
+    {
+      s_watcher = new QFileSystemWatcher;
+    }
+    QObject::connect(s_watcher, SIGNAL(fileChanged(const QString&)), this, SLOT(fileChanged(const QString&)));
+    if(not m_url.toLocalFile().isEmpty())
+    {
+      s_watcher->addPath(m_url.toLocalFile());
+    }
+  } else {
+    QObject::disconnect(s_watcher, SIGNAL(fileChanged(const QString&)), this, SLOT(fileChanged(const QString&)));
+    if(not m_url.toLocalFile().isEmpty())
+    {
+      s_watcher->removePath(m_url.toLocalFile());
+    }
+  }
+  m_monitorChanges = _v;
+  emit(monitorChangesChanged());
+}
+
+void FileIO::fileChanged(const QString& _name)
+{
+  if(_name == m_url.toLocalFile())
+  {
+    emit(fileChanged());
+  }
 }
--- FileIO.h
+++ FileIO.h
@@ -5,11 +5,14 @@
 
 #include <QUrl>
 
+class QFileSystemWatcher;
+
 class FileIO : public QObject
 {
   Q_OBJECT
   Q_PROPERTY(QString content READ content WRITE setContent NOTIFY contentChanged)
   Q_PROPERTY(QUrl url READ url WRITE setUrl NOTIFY urlChanged)
+  Q_PROPERTY(bool monitorChanges READ monitorChanges WRITE setMonitorChanges NOTIFY monitorChangesChanged)
 public:
   explicit FileIO(QObject *parent = 0);
   Q_INVOKABLE void readFile();
@@ -21,13 +24,21 @@ public:
   QString content() const { return m_content; }
   void setContent(const QString& _content) { if(m_content != _content) { m_content = _content; emit(contentChanged()); } }
   QUrl url() const { return m_url; }
-  void setUrl(const QUrl& _url) { m_url = _url; emit(urlChanged()); }
+  void setUrl(const QUrl& _url);
+  bool monitorChanges() const { return m_monitorChanges; }
+  void setMonitorChanges(bool _v);
+private slots:
+  void fileChanged(const QString& _name);
 signals:
   void contentChanged();
   void urlChanged();
+  void monitorChangesChanged();
+  void fileChanged();
 private:
   QString m_content;
   QUrl m_url;
+  bool m_monitorChanges = false;
+  static QFileSystemWatcher* s_watcher;
 };
 
 #endif // FILEIO_H
